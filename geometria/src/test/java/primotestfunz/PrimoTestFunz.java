90d907317c5637bca3583ef2dc5ddadfe62add95
alegenti <alessiagentilini@gmail.com>
2021-04-18 09:38:39 +0200

    file aggiunti

---
+++ geometria/src/test/java/primotestfunz/PrimoTestFunz.java
@@ -0,0 +1,34 @@
+package primotestfunz;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import java.time.Duration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrimoTestFunz {
+
+	@Test
+	public void test() {
+		
+		WebDriver driver = new FirefoxDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        try {
+            driver.get("https://google.com/ncr");
+            driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+            WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3")));
+            System.out.println(firstResult.getAttribute("textContent"));
+        } finally {
+            driver.quit();
+        }
+		
+	}
+
+}
